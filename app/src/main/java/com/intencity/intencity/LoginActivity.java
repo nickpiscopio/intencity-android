273a2b11992b43bc937756e604575ca7ba120b7d
nickpiscopio <nick.piscopio@gmail.com>
2015-12-09 18:39:18 -0500

    The initial commit for Intencity.

---
+++ app/src/main/java/com/intencity/intencity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.intencity.intencity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
