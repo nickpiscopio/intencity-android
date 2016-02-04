211741afe0103bb7faa01bbfea2ae957e86bec2d
nickpiscopio <nick.piscopio@gmail.com>
2016-02-04 09:39:37 -0500

    Fixed an issue that didn't show the user's first and last name in the search, but showed the default first and last name instead.

--- app/src/main/java/com/intencity/intencity/adapter/RankingListAdapter.java
+++ app/src/main/java/com/intencity/intencity/adapter/RankingListAdapter.java
@@ -100,7 +100,7 @@ public class RankingListAdapter extends ArrayAdapter<User>
             holder.name = (TextView) convertView.findViewById(R.id.text_view_name);
             holder.points = (TextView) convertView.findViewById(R.id.text_view_points);
             holder.totalBadgesTextView = (TextView) convertView.findViewById(R.id.total_badges);
-            
+
             holder.name.setText(user.getFirstName() + " " + user.getLastName());
             holder.points.setText(String.valueOf(user.getEarnedPoints()));
 
